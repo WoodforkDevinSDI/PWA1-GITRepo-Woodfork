963b7887965025146ff102d6672cdaf5712ebbcb
WoodforkDevinSDI <dmwoodfork@fullsail.edu>
2016-02-06 17:53:16 -0600

    Created Variables
    created the variables necessary for the program

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -1,5 +1,29 @@
-/**
- * Created by DevinWoodfork on 2/6/16.
+/*
+ Devin Woodfork
+ 2/6/26
+ Assignment: Goal1: Assignment: Due1
  */
 
 // console.log("test");
+
+//self-executing function
+
+(function(){
+
+console.log("FIGHT!!!");
+
+//player name
+    var playerOneName = "Scooby-Doo";
+    var playerTwoName = "Monsters";
+
+//player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+//player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    var round = 1;
+
+})();
