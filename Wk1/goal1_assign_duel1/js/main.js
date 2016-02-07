8dedf3b9f25dceec1b9fc0f5f0f785378bd4e44a
WoodforkDevinSDI <dmwoodfork@fullsail.edu>
2016-02-06 18:48:52 -0600

    Set Up The Functions
    Set Up the Functions for the program to work properly.

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -24,16 +24,63 @@
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    var round = 1;
+    var round = 0;
 
     function fight() {
     console.log('in the fight function');
-  
+
+    alert(playerOneName+":" +playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+
+    for (var i=0; i < 10; i++){
+        console.log(i);
+
+     var minDamage1 = player1Damage * .5;
+     var minDamage2 = player2Damage * .5;
+     var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+     var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+       // console.log(f1);
+       // console.log(f2);
+        playerOneHealth-=f1;
+        playerTwoHealth-=f2;
+
+        //console.log(playerOneHealth);
+        //console.log(playerTwoHealth);
+
+        console.log(playerOneName + ": " + playerOneHealth + playerTwoName+": " + playerTwoHealth);
+
+        var results = winnerCheck();
+        console.log(results);
+
+        if (results === "no winner") {
+            round++;
+            alert(playerOneName + ":" + playerOneHealth + " *ROUND " + round + " OVER*" + playerTwoName + ":" + playerTwoHealth);
+        }else{
+            alert(results);
+            break
+        }
+      };
+    };
+
+    function winnerCheck(){
+        console.log("in winnerCheck FN");
+        var result="no winner";
+
+        if (playerOneHealth<1 && playerTwoHealth<1) {
+            result = "You Both Die";
+        }else if(playerOneHealth <1){
+            result =playerTwoName + "WINS!!!"
+        }else if(playerTwoHealth <1){
+            result =playerOneName + "WINS!!!"
+        };
+
+        return result;
+
+
 
     };
 
-    function winnerCheck(){};
-    //code will go here
 
     /****** PROGRAM BEGINS ******/
     console.log("program starts");
