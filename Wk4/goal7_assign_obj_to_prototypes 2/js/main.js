836031b125f389fe48796ca6218d85f27eab3db0
WoodforkDevinSDI <dmwoodfork@fullsail.edu>
2016-03-02 21:53:07 -0600

    OBJECTS.Person Code Complete
    Completed the Pseudo code for the program

---
+++ Wk4/goal7_assign_obj_to_prototypes 2/js/main.js
@@ -0,0 +1,31 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+/* Devin M. Woodfork
+ 2/29/16
+ OBJECTS.Person Assignment
+ */
+
+//*********** PSEUDO CODE ONLY
+
+/*
+ runUpdate function at intervals of 30 times a second
+
+ use a for loop to cycle through person object's action attribute
+
+generate a random number and determine if the HTML rowcol is filled and with what
+
+if the rowcol is filled and with the random number value go to the next rowcol
+else place the random number value in that rowcol
+
+complete the if else statement a total of three times
+
+wait for the trigger
+
+end the runUpdate function until triggered
+
+
+
+ */
---
+++ Wk4/goal7_assign_obj_to_prototypes 2/js/person.js
@@ -0,0 +1,36 @@
+/* Devin M. Woodfork
+ 2/29/16
+ OBJECTS.Person Assignment
+ */
+
+//*********** PSEUDO CODE ONLY
+
+/* create new object for people including an array for jobs and actions
+
+   Person constructor should contain: name, job, action, row
+
+   name includes the name of the person
+
+   job includes the randomly selected job from jobs Array
+
+   action includes what person is doing the randomly selected action
+
+   row will be set in the Constructor
+
+   create an update function
+
+   Add the Person object to the global window "example: window.Person=Person"
+
+   Jobs Array for Person Object: Painter, Drawer, Singer, Dancer, Photographer
+
+   Actions Array for Person Object: Working, On Lunch, In Meeting, On the Phone, Driving
+
+   Constructor for new persons (name, row)
+
+   name: name of person
+   action: randomly selected from actions Array
+   job: randomly selected from jobs Array
+   row: appropriate HTML row
+
+   Update PROTOTYPE to change action of person
+ */
